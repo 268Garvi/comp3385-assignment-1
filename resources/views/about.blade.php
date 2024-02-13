7b0c1d82571a7d1fa5bbbe9d063766dbe82001af
Garvin Ponde <garvinponde@gmail.com>
2024-02-13 00:13:03 -0400

    css and html

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,7 @@
 @section('content')
 <h2>Garvin Ponde</h2>
 <p>I'm Garvin Ponde, a second-year computer science student. I've been interested in web development for a while, and I'm passionate about it because it allows me to create interactive and user-friendly websites that can make a positive impact on people's lives.
-             Web development provides endless opportunities for creativity and problem-solving, which I find incredibly exciting.</p>
+             Web development provides endless opportunities for creativity and problem-solving, which I find incredibly exciting.
+</p>
+<img src="{{ asset('images/image.jpg') }}" alt="My image" class="image1"/>
 @endsection
