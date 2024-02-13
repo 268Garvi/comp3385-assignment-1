cf0e5818e5f639164e2c56f80c5c9c75a3ddae14
Garvin Ponde <garvinponde@gmail.com>
2024-02-12 20:25:18 -0400

    My about page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view ("welcome");
+});
+
+
+Route::get('/about', function () {
+    return "My About Page";
 });
